9581d8535ae3e8e8b27ab1c7ea845cd1bfbde26d
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-05-12 16:05:05 +0100

    refactor: call authors-data.addAuthor() directly from the add-author component

--- src/app/components/add-author/add-author.component.ts
+++ src/app/components/add-author/add-author.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output }  from '@angular/core'; 
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Author } from '../../models/Author';
+import { AuthorsDataService } from '../../services/authors-data.service';
 
 @Component({
   selector: 'app-add-author',
@@ -10,18 +11,22 @@ import { Author } from '../../models/Author';
   styleUrl: './add-author.component.css'
 })
 export class AddAuthorComponent {
-  @Output() onSaveAuthor : EventEmitter<[string, number , string, string]> = new EventEmitter();
-  fullName! : string ;
-  year_Born! : number; 
-  technology! : string ; 
-  profile! : string ; 
-  @Output() authortmp!: [string, number , string, string] ;
-  
-  onSubmit(){
-    this.authortmp = [this.fullName, this.year_Born, this.technology, this.profile] ;
-    this.onSaveAuthor.emit(this.authortmp);
-    this.fullName ='';
-    this.technology ='';
+  fullName!: string;
+  year_Born!: number;
+  technology!: string;
+  profile!: string;
+
+  constructor(private authorsdataService: AuthorsDataService) {
+
+  }
+
+  onSubmit() {
+    this.authorsdataService.addAuthor(this.fullName, this.year_Born, this.technology, this.profile)
+      .subscribe((author) => {
+        console.log(author);
+      })
+    this.fullName = '';
+    this.technology = '';
     this.profile = '';
   }
 
